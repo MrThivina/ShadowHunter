e238c82c819a979c3d71d7c8004d2f6714dd59c0
Thivina Abhishek <107085510+MrThivina@users.noreply.github.com>
2026-05-05 15:20:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def domain_lookup():
 def menu():
     while True:
         print("""
-\033[91m[1]\033[91m\033[93m.\033[93m \033[92mUsername Search\033[92m
+\033[91m[\033[91m1\033[91m]\033[91m\033[93m.\033[93m \033[92mUsername Search\033[92m
 \033[91m[2]\033[91m\033[93m.\033[93m \033[92mInstagram Check\033[92m
 \033[91m[3]\033[91m\033[93m.\033[93m \033[92mDomain Lookup\033[92m
 \033[91m[4]\033[91m\033[93m.\033[93m \033[92mExit\033[92m
